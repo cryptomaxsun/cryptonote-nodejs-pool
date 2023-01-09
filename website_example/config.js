9c88df5dbc2ed20fbe66532efb2396169e9df82f
Maksim S <23011564+cryptomaxsun@users.noreply.github.com>
2023-01-09 19:47:27 +0200

    Update config.js
    Added network statistics.

--- website_example/config.js
+++ website_example/config.js
@@ -15,6 +15,16 @@ var transactionExplorer = "http://chainradar.com/{symbol}/transaction/{id}";
 var themeCss = "themes/default.css";
 var defaultLang = "en";
 
+var networkStat = {
+    "xfg": [
+              ["xfg.ms-pool.net.ua", "http://xfg.ms-pool.net.ua:8117"],
+              ["185.185.80.62/xfg/#","http://185.185.80.62:8207"],
+              ["loudmining.com/xfg/","https://loudmining.com/fuego/api"],
+              ["radioactive.sytes.net:8383/xfg/","http://radioactive.sytes.net:8383"]
+
+            ]
+};
+
 // Merged Mining:
 // var api = "http://poolhost/apiMerged";
 // var blockchainExplorer = "http://explorer.ird.cash/?hash={id}#block";
